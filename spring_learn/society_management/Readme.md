d6735193c34b22f2cbccdfccb93722aabd1c0cc4
Nitish <nitishg8879@gmail.com>
2026-05-21 18:15:10 +0530

    dff

---
+++ spring_learn/society_management/Readme.md
@@ -0,0 +1,4 @@
+trasiontal by saga pattern
+distrubutted logging
+in each service do multiple trasanction then call other service of txn
+circuit breaker,retry,rate limiter
--- spring_learn/society_management/members/src/main/java/com/societyManagement/members/controller/AdminController.java
+++ spring_learn/society_management/members/src/main/java/com/societyManagement/members/controller/AdminController.java
@@ -5,13 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/admin")
+@Slf4j
 public class AdminController {
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/sayHello")
     public String sayHello() {
+        log.info("Admin requested to say hello.");
         return "Hello, Admin!";
     }
 
